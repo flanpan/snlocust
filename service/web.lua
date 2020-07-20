68bd5fd83153526eff76615512a50646cbb93631
panfeng <panfeng@onemt.com.cn>
2020-07-20 21:22:10 +0800

    fix create counter/stats service bug

--- service/web.lua
+++ service/web.lua
@@ -1,8 +1,12 @@
 local skynet = require "skynet"
 local runner = require "runner"
 local dataset = require "dataset"
+local queue = require "skynet.queue"
+
+local lock
 
 function init()
+    lock = queue()
     runner.start(8001, 8002)
 end
 
@@ -10,12 +14,20 @@ function accept.broadcast(type, body)
     runner.broadcast(type, body)
 end
 
-function response.stats_service(...)
-    return dataset.stats_service(...)
+function response.stats_service(method, name)
+    local addr
+    lock(function()
+    addr = dataset.stats_service(method, name)
+    end)
+    return addr
 end
 
-function response.counter_service(...)
-    return dataset.counter_service(...)
+function response.counter_service(name)
+    local addr
+    lock(function()
+    addr = dataset.counter_service(name)
+    end)
+    return addr
 end
 
 function accept.report_stats(...)
