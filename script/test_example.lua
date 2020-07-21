7cbf739b819bb77c62366a3f3743c4ea76e90948
panfeng <panfeng@onemt.com.cn>
2020-07-21 11:24:52 +0800

    test_example use socketchannel

--- script/test_example.lua
+++ script/test_example.lua
@@ -3,107 +3,63 @@
 cd 3rd/skynet
 ./skynet examples/config
 --]]
-
-package.path = package.path .. '3rd/skynet/examples/?.lua'
-local socket = require "client.socket"
+package.path = package.path .. "3rd/skynet/examples/?.lua;"
 local proto = require "proto"
 local sproto = require "sproto"
 local util = require "common.util"
 local monitor = require "common.monitor"
 local skynet = require "skynet"
+local socket = require "skynet.socket"
+local sc = require "skynet.socketchannel"
 
-local host = sproto.new(proto.s2c):host "package"
-local request = host:attach(sproto.new(proto.c2s))
-
-local fd = assert(socket.connect(util.address()))
-
-local function send_package(fd, pack)
-	local package = string.pack(">s2", pack)
-	socket.send(fd, package)
-end
-
-local function unpack_package(text)
-	local size = #text
-	if size < 2 then
-		return nil, text
-	end
-	local s = text:byte(1) * 256 + text:byte(2)
-	if size < s+2 then
-		return nil, text
-	end
-
-	return text:sub(3,2+s), text:sub(3+s)
-end
-
-local function recv_package(last)
-	local result
-	result, last = unpack_package(last)
-	if result then
-		return result, last
-	end
-	local r = socket.recv(fd)
-	if not r then
-		return nil, last
-	end
-	if r == "" then
-		error "Server closed"
-	end
-	return unpack_package(last .. r)
-end
-
+local host, port = util.address()
+local sp = sproto.new(proto.s2c):host "package"
+local pack = sp:attach(sproto.new(proto.c2s))
 local session = 0
+local channel
+local read_response
 
-local function send_request(name, args)
+read_response = function(sock)
+    local sz = socket.header(sock:read(2))
+    local data = sock:read(sz)
+    local type, session, msg = sp:dispatch(data)
+    if type == 'RESPONSE' then
+        monitor.endtime(session, sz)
+        return session, true, msg
+    else 
+        --server notification
+        local name = session
+        monitor.incr(name)
+        return read_response(sock)
+    end
+end
+
+channel = sc.channel({host = host, port = port, nodelay = false, response = read_response})
+
+local function request(type, msg, resp)
 	session = session + 1
-	local str = request(name, args, session)
-    send_package(fd, str)
-    monitor.time('test', name, session)
+	local str = pack(type, msg or {}, session)
+    local package = string.pack(">s2", str)
+    monitor.time('test', type, session)
+    return channel:request(package, session)
 end
 
-local last = ""
+local t = {
+    set = function() request("set", { what = "hello", value = "world" }) end,
+    get = function() 
+        local res = request("get", { what = "hello" })
+        assert(res.result == 'world')
+    end
+}
 
-local function dispatch_package()
-	while true do
-		local v
-		v, last = recv_package(last)
-		if not v then
-			break
-        end
-        local size = #v
-        local type, session = host:dispatch(v)
-        if type == 'RESPONSE' then
-            monitor.endtime(session, size)
-        else
-            local name = session
-            monitor.incr(name)
-        end
-	end
-end
-
-local t = {}
-
-function t.set()
-    send_request("set", { what = "hello", value = "world" })
-end
-
-function t.get()
-    send_request("get", { what = "hello" })
-end
-
-function t.recv()
-    dispatch_package()
-end
-
-skynet.timeout(0, function()
-    send_request("handshake")
+skynet.fork(function()
+    channel:connect(false)
+    request('handshake')
 end)
 
 local fweight = {
     set = 1,
     get = 2,
-    recv = 10
 }
 
-util.run(t,fweight,0,1, function()
-    socket.close(fd)
-end)
+util.run(t,fweight,2,2)
